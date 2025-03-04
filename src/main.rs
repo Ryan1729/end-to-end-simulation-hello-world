80441ac4d3959370619ef46783024a45a265c131
Ryan1729 <Ryan1729@gmail.com>
2025-03-04 00:06:00 -0700

    Alternative design
    Whoops, missed this one before!

--- src/main.rs
+++ src/main.rs
@@ -79,12 +79,29 @@ fn translate_performance_Target100(balances: &[Money]) -> Performance {
     translate_performance_TargetBalance(balances, 100)
 }
 
-type DesignParameters = (Money,);
+type DesignParameters = (Money, Money);
+
+macro_rules! p {
+    ($_0: expr $(,)?) => {
+        ($_0, 0)
+    };
+    ($_0: expr, $_1: expr) => {
+        ($_0, $_1)
+    };
+}
 
 type DesignTranslator = fn (design_parameters: DesignParameters) -> Vec<Transaction>;
 
 fn translate_design_FortnightlyDeposit(design_parameters: DesignParameters) -> Vec<Transaction> {
     vec![t!(d, design_parameters.0); ANNUAL_FORTNIGHTS as _]
+}
+
+fn translate_design_InitialAndFortnightlyDeposit(design_parameters: DesignParameters) -> Vec<Transaction> {
+    let mut output = vec![t!(d, design_parameters.1); ANNUAL_FORTNIGHTS as usize + 1];
+
+    output[0] = t!(d, design_parameters.0);
+
+    output
 }
 
 fn performance_of_design(design_translator: DesignTranslator, design_parameters: DesignParameters) -> Performance {
@@ -107,7 +124,7 @@ macro_rules! evaluate {
 fn sample_performance_of_design(design_translator: DesignTranslator, design_variants: &[Money]) -> Vec<Performance> {
     design_variants
         .iter()
-        .map(|&m| performance_of_design(design_translator, (m,)))
+        .map(|&m| performance_of_design(design_translator, p!(m)))
         .collect()
 }
 
@@ -127,13 +144,17 @@ fn main() {
 
     println!("{:?}", translate_performance_Target100(&sb));
 
-    println!("{:?}", simulate_balance(&translate_design_FortnightlyDeposit((10,))));
+    println!("{:?}", simulate_balance(&translate_design_FortnightlyDeposit(p!(10,))));
 
-    let design_1 = (9,);
+    let design_1 = p!(9,);
 
     evaluate!(translate_design_FortnightlyDeposit, design_1);
 
     println!("{:?}", simulate_balance(&translate_design_FortnightlyDeposit(design_1)));
+
+    let design_2 = p!(90, 1);
+
+    println!("{:?}", simulate_balance(&translate_design_InitialAndFortnightlyDeposit(design_2)));
 
     let design_sweep = (0..16).collect::<Vec<_>>();
     let performances = sample_performance_of_design(translate_design_FortnightlyDeposit, &design_sweep);
